1aea0fdd0bd5b8530b6b4b6dc49b3b07929022fb
Blessing <ayeniblessing32@gmail.com>
2017-06-06 05:47:47 +0100

    Added test to return an empty array for empty array

--- app/library.js
+++ app/library.js
@@ -11,6 +11,28 @@ module.exports = {
         {
             return [];
         }
+        else 
+        {
+            let primeNumbers = [];
+
+            for(let i = 3; i < n; i++)
+            {
+                notPrime = primeNumbers.some(checkDivisibleNumber);
+                if(!notPrime)
+                {
+                    primeNumbers.push(i);
+                }
+            }
+
+        }
+
+        function checkDivisibleNumber(j)
+        {
+            if i % j === 0
+            {
+                return primeNumbers;
+            }
+        }
 
 
     }

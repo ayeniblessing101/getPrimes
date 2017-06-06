a959eaea702eeb4b542c7ed4582e47098ba3eb35
Blessing <ayeniblessing32@gmail.com>
2017-06-06 11:23:19 +0100

    modified function getPrimes

--- app/library.js
+++ app/library.js
@@ -1,29 +1,28 @@
 'use strict';
 module.exports = {
-
-    getPrimes: (n) => {
-
-        if(isNaN(n))
-        {
-            return "invalid argument";
+    
+    function getPrimes(n){
+      var primeNumbers = [];
+      if(isNaN(n))
+      {
+        return "invalid argument";
+      }
+      else if(n < 2)
+     {
+       return [];
+     }
+     for (var counter = 0; counter <= n; counter++){
+        for (var i = 2; i <= counter; i++){
+          if(counter === i){
+            primeNumbers.push(i);
+          }
+          if(counter % i === 0){
+            break;
+          }
         }
-        else if(n < 2)
-        {
-            return [];
-        }
-        else 
-        {
-            var primeNumbers = [];
-
-            for(var i = 2; i < n; i++)
-            {
-               if(n % i === 0)
-               {
-
-               } 
-            }
-            return primeNumbers.push(i);
-        }
+      }
+      return arr;
     }
 
 }
+
